c87566e74d591fe9a7acfcfb0ec8551028450b04
kanggyeonggu <rudrn0110@naver.com>
2026-02-09 21:05:53 +0900

    chore: docs 서브모듈 업데이트

--- src/main/java/com/lol/backend/modules/shop/dto/BanPickRequest.java
+++
@@ -1,9 +0,0 @@
-package com.lol.backend.modules.shop.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record BanPickRequest(
-    @NotBlank(message = "algorithmId는 필수입니다")
-    String algorithmId
-) {
-}
